cae8f46ca84e3ee4506d75030c454ec7324626a8
Bee <lazybee27102@gmail.com>
2017-07-24 12:01:00 -0700

    Nomorlize datbase v2

--- dbCuaHangNuocGiaiKhat -v2.sql
+++ dbCuaHangNuocGiaiKhat -v2.sql
@@ -13,13 +13,15 @@ go
 
 CREATE TABLE NHACUNGCAP -- nhà cung cấp
 (
-	MaNhaCungCap	int identity primary key ,
+	MaNhaCungCap	int identity primary key not null,
 	TenNhaCungCap	nvarchar(100),
 	DiaChiNhaCungCap nvarchar(200),
 	SoDienThoai int,
 	GhiChu nvarchar(500),
 )
+
 go
+
 CREATE TABLE LOAISANPHAM -- loại sản phẩm
 (
 	MaLoaiSanPham	int identity,
@@ -28,154 +30,74 @@ CREATE TABLE LOAISANPHAM -- loại sản phẩm
 )
 
 go
+
 CREATE TABLE DONVITINH -- đơn vị tính cho sản phẩm
 (
 	DonViTinh nvarchar(100) primary key,
 )
+
 go
-CREATE TABLE SANPHAM -- sản phẩm
-(
-	MaSanPham int  identity,
-	MaLoaiSanPham int  ,
-	TenSanPham nvarchar(100),
-	DonGia	money,
-	DonViTinh nvarchar(100) references DONVITINH(DonViTinh),
-	SoLuongTon	bigint,
-	DuocPhaChe bit ,
-	primary key (MaSanPham),
-	foreign key (MaLoaiSanPham) references LOAISANPHAM(MaLoaiSanPham),
-)
-go
-CREATE TABLE SANPHAM_NHACUNGCAP -- sản phẩm _ nhà cung cấp
-(
-	MaSanPham	int ,
-	MaNhaCungCap	int,
-	foreign key (MaSanPham) references SANPHAM(MaSanPham),
-	foreign key (MaNhaCungCap) references NHACUNGCAP(MaNhaCungCap),
-)
-go
+
 CREATE TABLE NGUYENLIEU -- nguyên liệu
 (
 	MaNguyenLieu int  identity,
 	TenNguyenLieu nvarchar(100),
 	SoLuongTon	bigint,
+	GiaNhap money,
 	DonViTinh nvarchar(100) references DONVITINH(DonViTinh),
 	primary key (MaNguyenLieu),
 )
+
 go
+
+CREATE TABLE SANPHAM -- sản phẩm
+(
+	MaSanPham int  identity primary key not null,
+	MaLoaiSanPham int  ,
+	TenSanPham nvarchar(100),
+	GiaTien	money,
+	GhiChu nvarchar(200),
+	foreign key (MaLoaiSanPham) references LOAISANPHAM(MaLoaiSanPham)
+)
+
+go
+
+CREATE TABLE CHITIET_SANPHAM
+(
+	MaSanPham int,
+	MaNguyenLieu int,
+	SoLuong int,
+	primary key (MaSanPham, MaNguyenLieu),
+	foreign key (MaSanPham) references SANPHAM(MaSanPham),
+	foreign key (MaNguyenLieu) references NGUYENLIEU(MaNguyenLieu)
+)
+
+go
+
 CREATE TABLE NGUYENLIEU_NHACUNGCAP -- nguyên liệu _ nhà cung cấp
 (
-	MaNguyenLieu	int ,
+	MaNguyenLieu	int,
 	MaNhaCungCap	int,
+	primary key(MaNguyenLieu, MaNhaCungCap),
 	foreign key (MaNguyenLieu) references NGUYENLIEU(MaNguyenLieu),
 	foreign key (MaNhaCungCap) references NHACUNGCAP(MaNhaCungCap),
 )
-go
-CREATE TABLE PHACHE -- pha chế
-(
-	MaPhaChe int identity primary key ,
-	MaSanPham	int not null unique,
-	foreign key (MaSanPham) references SANPHAM(MaSanPham),
-)
-go
-CREATE TABLE CHITIET_PHACHE -- chi tiết pha chế
-(
-	MaPhaChe int ,
-	MaNguyenLieu int  ,
-	SoLuongNguyenLieuPhaChe bigint ,
-	foreign key (MaPhaChe) references PHACHE(MaPhaChe),
-	foreign key (MaNguyenLieu) references NGUYENLIEU(MaNguyenLieu),
-)
 
 go
+
 create table NHANVIEN -- nhân viên
 (
-MaNhanVien int identity primary key,
-TenNhanVien nvarchar(200),
-DiaChi nvarchar(200),
-Email nvarchar(50),
-SoDienThoai int,
-GioiTinh	nvarchar(50),
-TinhTrang bit
-)
-go
-CREATE TABLE PHIEUNHAP -- phiếu nhập
-(
-	MaPhieuNhap int identity primary key not null,
-	NgayLap datetime,
-	MaNhanVien int,
-	TongTien money,
-	foreign key (MaNhanVien) references NHANVIEN(MaNhanVien),
-)
-go
-CREATE TABLE CHITIET_PHIEUNHAP_SANPHAM -- chi tiết phiếu nhập cho sản phẩm
-(
-	MaPhieuNhap int  ,
-	MaSanPham int ,
-	DonViTinh nvarchar(100),
-	SoLuongNhap int,
-	DonGiaNhap	money,
-	SoLuongQuyDoi int,
-	ThanhTien as SoLuongNhap*DonGiaNhap,
-	primary key (MaPhieuNhap,MaSanPham),
-	foreign key (MaPhieuNhap) references PHIEUNHAP(MaPhieuNhap),
-	foreign key (DonViTinh) references DONVITINH(DonViTinh),
-	foreign key (MaSanPham) references SANPHAM(MaSanPham)
-)
-go
-CREATE TABLE CHITIET_PHIEUNHAP_NGUYENLIEU -- chi tiết phiếu nhập cho nguyên liệu
-(
-	MaPhieuNhap int  ,
-	MaNguyenLieu int ,
-	DonViTinh nvarchar(100),
-	SoLuongNhap int,
-	DonGiaNhap	money,
-	SoLuongQuyDoi int,
-	ThanhTien as SoLuongNhap*DonGiaNhap,
-	primary key (MaPhieuNhap,MaNguyenLieu),
-	foreign key (MaPhieuNhap) references PHIEUNHAP(MaPhieuNhap),
-	foreign key (DonViTinh) references DONVITINH(DonViTinh),
-	foreign key (MaNguyenLieu) references NGUYENLIEU(MaNguyenLieu)
-)
-go
-CREATE TABLE HOADON -- hoá đơn thanh toán 
-(
-	MaHoaDon nvarchar(200) primary key,
-	MaNhanVien int,
-	NgayLap datetime ,
-	TongTien money,
-	foreign key (MaNhanVien) references NHANVIEN(MaNhanVien),
-)
-go
-CREATE TABLE CHITIET_HOADON -- chi tiết hoá đơn
-(
-	MaHoaDon nvarchar(200)  ,
-	MaSanPham int ,
-	SoLuong int ,
-	DonGia money,
-	ThanhTien as SoLuong*DonGia,
-	foreign key (MaSanPham) references SANPHAM(MaSanPham),
-	foreign key (MaHoaDon) references HOADON(MaHoaDon),
+	MaNhanVien int identity primary key not null,
+	TenNhanVien nvarchar(200),
+	DiaChi nvarchar(200),
+	Email nvarchar(50),
+	SoDienThoai int,
+	GioiTinh	nvarchar(50),
+	TinhTrang bit
 )
 
 go
-CREATE TABLE SOCHO -- số chờ
-(
-	MaSoCho int  identity primary key,
-	TinhTrang bit
-)
-go
-CREATE TABLE PHIEUPHACHE -- phiếu yêu cầu pha chế
-(
-	MaPhieuPhaChe nvarchar(200)  primary key,
-	MaSoCho int ,
-	MaHoaDon nvarchar(200) ,
-	NgayLap datetime,
-	TinhTrang bit ,
-	foreign key (MaSoCho) references SOCHO(MaSoCho),
-	foreign key (MaHoaDon) references HOADON(MaHoaDon),
-)
-go
+
 CREATE TABLE PHIEUMUAHANG 
 (
 	MaPhieuMuaHang int identity primary key not null,
@@ -185,31 +107,88 @@ CREATE TABLE PHIEUMUAHANG
 	TinhTrang bit,
 	foreign key (MaNhanVien) references NHANVIEN(MaNhanVien),
 )
+
 go
+
 CREATE TABLE CHITIET_PHIEUMUAHANG
 (
-	MaPhieuMuaHang int  ,
-	TenHangHoa nvarchar(500),
-	DonViTinh nvarchar(100),
+	MaPhieuMuaHang int,
+	MaNguyenLieu int,
 	SoLuong int,
-	DonGia	money,
-	ThanhTien as SoLuong*DonGia,
-	primary key (MaPhieuMuaHang),
+	primary key (MaPhieuMuaHang, MaNguyenLieu),
 	foreign key (MaPhieuMuaHang) references PHIEUMUAHANG(MaPhieuMuaHang),
-	foreign key (DonViTinh) references DONVITINH(DonViTinh),
+	foreign key (MaNguyenLieu) references NGUYENLIEU(MaNguyenLieu)
 )
+
 go
+
+CREATE TABLE PHIEUNHAP -- phiếu nhập
+(
+	MaPhieuNhap int identity primary key not null,
+	MaPhieuMuaHang int,
+	NgayLap datetime,
+	MaNhanVien int,
+	TongTien money,
+	foreign key (MaNhanVien) references NHANVIEN(MaNhanVien),
+	foreign key (MaPhieuMuaHang) references PHIEUMUAHANG(MaPhieuMuaHang)
+)
+
+go
+
+CREATE TABLE HOADON -- hoá đơn thanh toán 
+(
+	MaHoaDon int primary key not null,
+	MaNhanVien int,
+	NgayLap datetime,
+	TongTien money,
+	foreign key (MaNhanVien) references NHANVIEN(MaNhanVien),
+)
+
+go
+
+CREATE TABLE CHITIET_HOADON -- chi tiết hoá đơn
+(
+	MaHoaDon int,
+	MaSanPham int,
+	SoLuong int,
+	primary key (MaHoaDon, MaSanPham),
+	foreign key (MaSanPham) references SANPHAM(MaSanPham),
+	foreign key (MaHoaDon) references HOADON(MaHoaDon),
+)
+
+go
+
+CREATE TABLE SOCHO -- số chờ
+(
+	MaSoCho int  identity primary key,
+	TinhTrang bit
+)
+
+go
+
+CREATE TABLE PHIEUPHACHE -- phiếu yêu cầu pha chế
+(
+	MaPhieuPhaChe int primary key not null,
+	MaSoCho int ,
+	MaHoaDon int  ,
+	NgayLap datetime,
+	TinhTrang bit ,
+	foreign key (MaSoCho) references SOCHO(MaSoCho),
+	foreign key (MaHoaDon) references HOADON(MaHoaDon),
+)
+
+go
+
 CREATE TABLE PHIEUCHI
 (
 	MaPhieuChi int identity primary key not null,
+	MaPhieuNhap int, 
 	MaNhanVien int,
 	NgayLap datetime,
-	TenNhaCungCap nvarchar(100),
-	DiaChi nvarchar(500),
-	NguoiNhanTien nvarchar(100),
 	LyDoChi nvarchar(1000),
 	TongTienChi money,
 	foreign key (MaNhanVien) references NHANVIEN(MaNhanVien),
+	foreign key (MaPhieuNhap) references PHIEUNHAP(MaPhieuNhap)
 )
 
 
